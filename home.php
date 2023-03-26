771973c07838ee2d3249c6696e76f808491ae570
dhitahe <widya.anindita89@gmail.com>
2023-03-27 01:23:33 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>home</title>
+</head>
+<body>
+
+<h1> Sistem Informasi Aparatur Negeri Sipil</h1>
+
+	<form action="" method="post">
+
+		<ul>
+		
+			<li>
+				<button type="submit" name="login">LOGIN</button>
+			</li> 
+		
+		</ul>
+	</form>
+
+</body>
+</html>
